90296e721818a7f4fb3e33fdb8ee12f8e07425ac
Lars Morten Bek <lars@beksoft.dk>
2023-01-09 13:34:17 +0100

    port_test.go: Added * added TestPortIsUsed that tests the portIsUsed method in net package

---
+++ tests/utils/net/port_test.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"github.com/NineNineFive/go-local-web-gui/utils/net"
+	"strconv"
+	"testing"
+)
+
+func BenchmarkGetInt(benchmark *testing.B) {
+	// Reset the timer before running the code
+	benchmark.ResetTimer()
+
+	// Run the code b.N times
+	for i := 0; i < benchmark.N; i++ {
+		for j := 11450; j < 11453; j++ {
+			net.IsPortUsed("localhost", strconv.Itoa(j))
+		}
+	}
+}
+
+func TestPortIsUsed(test *testing.T) {
+	for i := 11450; i < 11453; i++ {
+		net.IsPortUsed("localhost", strconv.Itoa(i))
+	}
+}
