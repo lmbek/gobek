ce9c6951710dc6f172f92c21f7cfb840608c4471
Lars Morten Bek <lars@beksoft.dk>
2023-07-12 09:11:28 +0200

    launcher_test.go: Modified * Updated tests because of concurrent testing problem (chrome opening and closing, which could conflict). Fixed by adding a 1 second delay to some of the test (before they start) * Added a test with StartDefault

--- tests/launcher/launcher_test.go
+++ tests/launcher/launcher_test.go
@@ -11,8 +11,49 @@ import (
 	"time"
 )
 
+// main.go edition (to show methods that can be used in your own main.go)
+func TestMainStart(test *testing.T) {
+	waitgroup := &sync.WaitGroup{}
+	waitgroup.Add(1)
+
+	// shutdown for test after 3 seconds
+	go func() {
+		// shutdown app after 3 seconds
+		time.Sleep(time.Second * 3)
+		err := gobek.Shutdown()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		waitgroup.Done()
+	}()
+
+	// CODE STARTS HERE
+	// For windows we need a organisation name and project name
+	var organisationName = "NewOrganisationName" // put in organisation name
+	var projectName = "NewProjectName"           // put in project name
+
+	var frontendPath = "./../_frontend" // this should be set to where frontend files is (frontend folder: html, css, javascript...)
+
+	var chromeLauncher = gobek.ChromeLauncher{
+		Location:                os.Getenv("programfiles") + "\\Google\\Chrome\\Application\\chrome.exe",
+		FrontendInstallLocation: os.Getenv("localappdata") + "\\Google\\Chrome\\InstalledApps\\" + organisationName + "\\" + projectName,
+	}
+
+	var chromiumLauncher = gobek.ChromiumLauncher{
+		Location: "/var/lib/snapd/desktop/applications/chromium_chromium.desktop", // TODO: check if better location or can be customised
+	}
+
+	result := gobek.StartDefault(frontendPath, chromeLauncher, chromiumLauncher).Error()
+
+	expected := errors.New("http: Server closed").Error()
+	helpers.StandardTestChecking(test, result, expected)
+	waitgroup.Wait()
+}
+
 // SIMPLE VERSION
 func TestStart(test *testing.T) {
+	time.Sleep(time.Second * 1) // wait 1 second because this test fails bc of concurrent operations
 	//tests.PrintGotFatalError(test, "NOT IMPLEMENTED YET!")
 	waitgroup := &sync.WaitGroup{}
 	waitgroup.Add(1)
@@ -39,6 +80,7 @@ func TestStart(test *testing.T) {
 }
 
 func TestStart2(test *testing.T) {
+	time.Sleep(time.Second * 1) // wait 1 second because this test fails bc of concurrent operations
 	//tests.PrintGotFatalError(test, "NOT IMPLEMENTED YET!")
 	waitgroup := &sync.WaitGroup{}
 	waitgroup.Add(1)
@@ -65,6 +107,7 @@ func TestStart2(test *testing.T) {
 
 // ADVANCED VERSION
 func TestStartWithCustomSettings(test *testing.T) {
+	time.Sleep(time.Second * 1) // wait 1 second because this test fails bc of concurrent operations
 	//tests.PrintGotFatalError(test, "NOT IMPLEMENTED YET!")
 	waitgroup := &sync.WaitGroup{}
 	waitgroup.Add(1)
